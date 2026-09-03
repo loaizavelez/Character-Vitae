0bb270a30a3659ccf730cfeb89fd7d1cab3d0b80
loaizavelez <miguel.loaizav@upb.edu.co>
2026-09-03 15:09:55 -0500

    cambio de nombre e imagen de abeja

--- src/Componentes/BeeAnimator.jsx
+++ src/Componentes/BeeAnimator.jsx
@@ -1,3 +1,3 @@
 import React, { useEffect, useRef } from 'react';
 import  gsap  from 'gsap';
-import beeImage from '../assets/bee.png'; // Asegúrate de tener una imagen de abeja en la carpeta assets
+import beeImage from '../assets/bee.jpg'; // Asegúrate de tener una imagen de abeja en la carpeta assets
