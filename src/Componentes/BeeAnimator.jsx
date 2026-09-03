8396da12b30e2f254546277b71c36f0298f6bc62
loaizavelez <miguel.loaizav@upb.edu.co>
2026-09-03 15:02:09 -0500

    si

---
+++ src/Componentes/BeeAnimator.jsx
@@ -0,0 +1,3 @@
+import React, { useEffect, useRef } from 'react';
+import  gsap  from 'gsap';
+import beeImage from '../assets/bee.png'; // Asegúrate de tener una imagen de abeja en la carpeta assets
